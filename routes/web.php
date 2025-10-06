e569e3b034f3fe28e075089b3f2da5232c172da2
Louis-VR <134929961+louvr197@users.noreply.github.com>
2025-10-06 15:57:37 +0200

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\BattleController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\VoteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/',[BattleController::class,'index']);
+Route::get('/battles/{battle}', [BattleController::class, 'show'])->name('front.battles.show');
+Route::post('/votes', [VoteController::class, 'store'])->name('votes.store')->middleware('auth');
+
+require __DIR__ . '/auth.php';
